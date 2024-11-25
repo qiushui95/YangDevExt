b088c489572636d3fb81f874127061ce082198e0
YCX-C <ycx974577817@gmail.com>
2024-11-25 19:00:10 +0800

    VMAction.invoke修改为协程方法

--- vm/core/src/main/java/zzz/me/yang/dev/ext/vm/core/action/VMAction.kt
+++ vm/core/src/main/java/zzz/me/yang/dev/ext/vm/core/action/VMAction.kt
@@ -10,5 +10,5 @@ public abstract class VMAction : MVIAction {
         return owner
     }
 
-    public abstract operator fun invoke(owner: LifecycleOwner?)
+    public abstract suspend operator fun invoke(owner: LifecycleOwner?)
 }
