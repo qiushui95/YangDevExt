3fdc46d190605a9127573635c01b68bf1288afb4
YCX-C <ycx974577817@gmail.com>
2025-02-20 16:23:27 +0800

    优化FragmentResult

---
+++ fragment/src/main/java/zzz/me/yang/dev/ext/fragment/FragmentResultListener.kt
@@ -0,0 +1,7 @@
+package zzz.me.yang.dev.ext.fragment
+
+import android.os.Bundle
+
+public fun interface FragmentResultListener {
+    public fun onReceive(bundle: Bundle)
+}
--- fragment/src/main/java/zzz/me/yang/dev/ext/fragment/FragmentResultUtils.kt
+++ fragment/src/main/java/zzz/me/yang/dev/ext/fragment/FragmentResultUtils.kt
@@ -6,7 +6,10 @@ import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+
+private typealias ListenerMap = MutableMap<String, MutableSet<FragmentResultListener>>
 
 public object FragmentResultUtils {
     public const val REQUEST_KEY: String = "fragmentResultRequestKey"
@@ -50,38 +53,100 @@ public object FragmentResultUtils {
         }
     }
 
-    public inline fun <reified T : Parcelable> getResult(bundle: Bundle): T? {
+    public fun getResult(bundle: Bundle): Parcelable? {
         return getParcelable(bundle, KEY_RESULT)
     }
 
-    public inline fun <reified T : Parcelable> getResultNotNull(bundle: Bundle): T {
-        return getResult<T>(bundle) ?: throw NullPointerException()
+    private val listenerMap by lazy { mutableMapOf<String, MutableSet<FragmentResultListener>>() }
+
+    @Synchronized
+    private fun doWithListenerMap(block: (ListenerMap) -> Unit) {
+        block(listenerMap)
     }
 
-    public inline fun <reified T : Parcelable> setListener(
+    private fun getListenerKey(
         manager: FragmentManager,
         owner: LifecycleOwner,
-        requestKey: String = REQUEST_KEY,
-        crossinline listener: (T?) -> Unit,
-    ) {
-        manager.setFragmentResultListener(requestKey, owner) { _, bundle ->
-            listener(getResult<T>(bundle))
+        requestKey: String,
+    ): String {
+        val managerId = System.identityHashCode(manager)
+        val ownerId = System.identityHashCode(owner)
+
+        return "${requestKey}_${managerId}_$ownerId"
+    }
+
+    private val lifecycleObserver by lazy {
+        object : DefaultLifecycleObserver {
+            override fun onDestroy(owner: LifecycleOwner) {
+                val ownerId = "_${System.identityHashCode(owner)}_"
+
+                doWithListenerMap { map ->
+                    for (key in map.keys) {
+                        if (ownerId !in key) continue
+
+                        map.remove(key)
+                    }
+
+                    val iterator = map.iterator()
+
+                    while (iterator.hasNext()) {
+                        val (key, _) = iterator.next()
+
+                        if (ownerId !in key) continue
+
+                        iterator.remove()
+                    }
+                }
+            }
         }
     }
 
-    public inline fun <reified T : Parcelable> setListener(
-        fragment: Fragment,
-        requestKey: String = REQUEST_KEY,
-        crossinline listener: (T?) -> Unit,
-    ) {
-        setListener<T>(fragment.childFragmentManager, fragment, requestKey, listener)
+    public fun removeListener(listener: FragmentResultListener) {
+        doWithListenerMap { map ->
+            val iterator = map.iterator()
+
+            while (iterator.hasNext()) {
+                val (_, set) = iterator.next()
+
+                set.remove(listener)
+
+                if (set.isNotEmpty()) continue
+
+                iterator.remove()
+            }
+        }
     }
 
-    public inline fun <reified T : Parcelable> setListener(
+    public fun setListener(
+        manager: FragmentManager,
+        owner: LifecycleOwner,
+        requestKey: String = REQUEST_KEY,
+        listener: FragmentResultListener,
+    ) {
+        val listenerKey = getListenerKey(manager, owner, requestKey)
+
+        doWithListenerMap { it.getOrPut(listenerKey) { mutableSetOf() }.add(listener) }
+
+        owner.lifecycle.addObserver(lifecycleObserver)
+
+        manager.setFragmentResultListener(requestKey, owner) { _, bundle ->
+            doWithListenerMap { map -> map[listenerKey]?.forEach { it.onReceive(bundle) } }
+        }
+    }
+
+    public fun setListener(
+        fragment: Fragment,
+        requestKey: String = REQUEST_KEY,
+        listener: FragmentResultListener,
+    ) {
+        setListener(fragment.childFragmentManager, fragment, requestKey, listener)
+    }
+
+    public fun setListener(
         activity: FragmentActivity,
         requestKey: String = REQUEST_KEY,
-        crossinline listener: (T?) -> Unit,
+        listener: FragmentResultListener,
     ) {
-        setListener<T>(activity.supportFragmentManager, activity, requestKey, listener)
+        setListener(activity.supportFragmentManager, activity, requestKey, listener)
     }
 }
