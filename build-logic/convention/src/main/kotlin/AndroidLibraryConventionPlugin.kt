2f930677eb7aaacb13fe40556815a939dd0f3787
YCX-C <ycx974577817@gmail.com>
2026-04-01 14:59:26 +0800

    新增Hero maven发布源

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -6,8 +6,10 @@ import nbe.someone.code.configureKotlinAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.publish.PublishingExtension
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.getByType
+import java.net.URI
 
 @Suppress("unused")
 class AndroidLibraryConventionPlugin : Plugin<Project> {
@@ -25,6 +27,19 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
             extensions.configure<LibraryExtension> {
                 configureKotlinAndroid(this)
                 defaultConfig.consumerProguardFiles("consumer-rules.pro")
+            }
+
+            extensions.configure<PublishingExtension> {
+                repositories {
+                    maven {
+                        name = "Hero"
+                        url = URI.create(System.getenv("HERO_NEXUS_URL"))
+                        credentials {
+                            username = System.getenv("HERO_NEXUS_USERNAME")
+                            password = System.getenv("HERO_NEXUS_PASSWORD")
+                        }
+                    }
+                }
             }
 
             configSpotless()
--- build-logic/convention/src/main/kotlin/KotlinLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/KotlinLibraryConventionPlugin.kt
@@ -3,12 +3,14 @@ import nbe.someone.code.configSpotless
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.named
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import java.net.URI
 
 @Suppress("unused")
 class KotlinLibraryConventionPlugin : Plugin<Project> {
@@ -28,6 +30,19 @@ class KotlinLibraryConventionPlugin : Plugin<Project> {
                 compilerOptions {
                     jvmTarget.set(JvmTarget.JVM_21)
                     freeCompilerArgs.add("-Xexplicit-api=strict")
+                }
+            }
+
+            extensions.configure<PublishingExtension> {
+                repositories {
+                    maven {
+                        name = "Hero"
+                        url = URI.create(System.getenv("HERO_NEXUS_URL"))
+                        credentials {
+                            username = System.getenv("HERO_NEXUS_USERNAME")
+                            password = System.getenv("HERO_NEXUS_PASSWORD")
+                        }
+                    }
                 }
             }
 
